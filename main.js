a143cacb8da2a85682ed9607eaed0b9fa8064314
Darran Rimron <darran.rimron@beatsystems.com>
2015-07-24 16:03:31 +0100

    everything up to the actual packet for ping :)
    -Dx

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+var nconf = require('nconf'),
+    crontab = require('node-crontab'),
+    monitor = require('./lib/monitor');
+
+var app = this;
+
+var conffile = null;
+
+var argv = process.argv.slice(2);
+
+if(argv.length)
+    conffile = argv.pop();
+
+nconf.argv()
+    .env()
+    .file({ file: conffile })
+    .load();
+
+var tests = nconf.get('tests');
+
+// die without tests
+if(typeof tests == 'undefined') {
+    console.error('NO TESTS FOUND');
+    process.exit(-1);
+}
+
+tests.forEach(function(testdata) {
+    testdata.jobId = crontab.scheduleJob(testdata.cron, function(testdata){
+        console.log('testTime');
+        console.info(monitor(testdata));
+        console.log('end of testTime');
+    }, [testdata], app);
+
+    console.log(testdata.name+" scheduled as jobId "+testdata.jobId);
+});
