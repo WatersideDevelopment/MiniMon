d4d4f71b9751937b99f5c4c45e430bee45bdf27c
Darran Rimron <darran.rimron@beatsystems.com>
2015-07-29 15:58:42 +0100

    Adn remove the tonne of crappy debug data that you left behind. FOOL.
    -Dx

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
 var nconf = require('nconf'),
     minimon = require('./embedded');
 
-var pointless = require('./embedded');
-var app = this;
-
 var conffile = null;
 
 var argv = process.argv.slice(2);
